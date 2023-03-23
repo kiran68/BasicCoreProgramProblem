1ee87ae0f5facb6a10e53bf27049bffc61c4afc9
kiran68 <kiranurshil88@gmail.com>
2023-03-23 21:51:35 +0530

    Add:Prime Number

---
+++ PrimeNumber.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.logicalproblem;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number :");
+		int num = sc.nextInt();
+		if (isPrime(num)) {
+			System.out.println("It Is Prime Number:");
+		} else {
+			System.out.println("It is Not Prime Number");
+		}
+	}
+
+	public static boolean isPrime(int num) {
+		if (num <= 1)
+			return false;
+		for (int i = 2; i < Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
